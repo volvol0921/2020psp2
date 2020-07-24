dcec94d60f1be8ef26facadf79e72b0044313839
volvol0921 <happysmile0921@gmail.com>
2020-07-25 08:49:18 +0900

    1回目

--- k03/k03.c
+++ k03/k03.c
@@ -12,17 +12,33 @@
 extern double r_unif(void);
 extern double r_stdnorm(void);
 
-int main(int argc, char* argv[])
-{
-    int num_dummy;
+static void print_usage(char const *progname) {
+    fprintf(stderr, "usage: %s mu sigma N\n", progname);
+}
+
+int main(int argc, char* argv[]){
+    if (argc < 4) {
+        print_usage(argv[0]);
+        return 1;
+    }
+
     double mu;
+    if (sscanf(argv[1], "%lf", &mu) != 1) {
+        print_usage(argv[0]);
+        return 1;
+    }
+
     double sigma;
-    int i;
-    double dummy;
+    if (sscanf(argv[2], "%lf", &sigma) != 1) {
+        print_usage(argv[0]);
+        return 1;
+    }
 
-
-
-
+    int num_dummy;
+    if (sscanf(argv[3], "%d", &num_dummy) != 1) {
+        print_usage(argv[0]);
+        return 1;
+    }
 
     printf("============================================\n");
     printf("template mean: %4.1lf\n",mu);
@@ -31,9 +47,9 @@ int main(int argc, char* argv[])
     printf("============================================\n");
 
     srand(RAND_SEED);
-    for(  ){
+    for(int i = 0; i < num_dummy; ++i){
         /* r_stdnormを使って，1人のデータを捏造 */
-        dummy =   ;
+        double dummy = r_stdnorm() * sigma + mu;
         printf("%5.2lf\n",dummy);
     }
 
